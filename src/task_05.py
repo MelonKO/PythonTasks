7a9ba4eafc8a4d1e7a1b35c46ea87ef6d9c325f0
MelonKO <melon654@gmail.com>
2025-05-08 23:28:41 +0300

    Adds proper test for task_05

--- src/task_05.py
+++ src/task_05.py
@@ -1,19 +1,18 @@
 # Разработать метод date_in_future(integer), который вернет дату через integer дней.
 # Если integer не является целым числом, то метод должен вывести текущую дату.
-# Формат возвращаемой методом даты должен иметь следующий вид '24-03-2001
-# 22:33:44'.
+# Формат возвращаемой методом даты должен иметь следующий вид '24-03-2001 22:33:44'.
 
 from datetime import datetime, timedelta
 from typing import Union
 
 
-def formatDateTime(date: datetime) -> str:
+def format_date_time(date: datetime) -> str:
     return date.strftime("%d-%m-%Y %H:%M:%S")
 
 
-def date_in_future(offest: Union[int, None]):
+def date_in_future(integer: Union[int, None]):
     now = datetime.now()
-    if not isinstance(offest, int):
-        return formatDateTime(now)
-    future_date = now + timedelta(days=offest)
-    return formatDateTime(future_date)
+    if not isinstance(integer, int):
+        return format_date_time(now)
+    future_date = now + timedelta(days=integer)
+    return format_date_time(future_date)
--- tests/test_task05.py
+++ tests/test_task05.py
@@ -1,19 +1,51 @@
+import datetime
 import unittest
-from src.task_05 import date_in_future
+from unittest.mock import patch
+from src.task_05 import date_in_future, format_date_time
 
 
-class DatTest(unittest.TestCase):
+class DateTest(unittest.TestCase):
 
     @classmethod
-    def setUpClass(cls) -> None:
-        print()
-        print("="*4, "TASK 05", "="*4)
-        return super().setUpClass()
+    def setUpClass(cls):
+        # Устанавливаем фиктивное текущее время
+        cls.fixed_time = datetime.datetime(2001, 3, 24, 22, 33, 44)
+        cls.fixed_time_str = format_date_time(cls.fixed_time)
+        # Подменяем datetime.now()
+        cls.patcher = patch('src.task_05.datetime')
+        cls.mock_datetime = cls.patcher.start()
+        cls.mock_datetime.now.return_value = cls.fixed_time
 
-    def test(self):
-        print("Yesterday:", date_in_future(-1))
-        print("Today:", date_in_future(None))
-        print("Tomorrow:", date_in_future(1))
+    @classmethod
+    def tearDownClass(cls):
+        # Восстанавливаем оригинальный datetime.datetime
+        cls.patcher.stop()
+
+    def test_print_format(self):
+        # 24-03-2001 22:33:44
+        self.assertEqual(format_date_time(
+            datetime.datetime(2001, 3, 24, 22, 33, 44)), "24-03-2001 22:33:44")
+
+    def test_non_integer_day(self):
+        self.assertEqual(date_in_future(None), self.fixed_time_str)
+        self.assertEqual(date_in_future([]), self.fixed_time_str)
+
+    def test_zero_days(self):
+        self.assertEqual(date_in_future(0), self.fixed_time_str)
+
+    def test_yesterday(self):
+        days_offset = 1
+        result = date_in_future(days_offset)
+        expected = format_date_time(
+            self.fixed_time + datetime.timedelta(days=days_offset))
+        self.assertEqual(result, expected)
+
+    def test_tomorrow(self):
+        days_offset = -1
+        result = date_in_future(days_offset)
+        expected = format_date_time(
+            self.fixed_time + datetime.timedelta(days=days_offset))
+        self.assertEqual(result, expected)
 
 
 if __name__ == "__main__":
